61570f9a0e675c06a69e0049511e96aa4cbb6f6d
Prarthana10 <74309706+Prarthana10@users.noreply.github.com>
2021-12-30 17:07:53 +0530

    Update uatg_csrbox_read_only_registers.py

--- uatg_csrbox_read_only_registers.py
+++ uatg_csrbox_read_only_registers.py
@@ -10,7 +10,7 @@ from typing import Dict, Union, Any, List
 import re
 import os
 
-class uatg_csrbox_read_only_registers.(IPlugin):
+class uatg_csrbox_read_only_registers(IPlugin):
 	def __init__(self):
 		super().__init__()
 		pass
