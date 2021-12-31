d57be64e45ed62a5dd4083f684d8fe2b6fae035e
Prarthana10 <74309706+Prarthana10@users.noreply.github.com>
2021-12-31 18:58:00 +0530

    Add files via upload

---
+++ uatg_csrbox_misa_c_ext.py
@@ -0,0 +1,57 @@
+
+from yapsy.IPlugin import IPlugin
+from ruamel.yaml import YAML
+import uatg.regex_formats as rf
+from typing import Dict, Union, Any, List
+import re
+import os
+
+class uatg_csrbox_misa_c_ext (IPlugin):
+	def __init__(self):
+		super().__init__()
+		pass
+		
+	def execute(self, core_yaml, isa_yaml):
+		self.isa = isa_yaml['hart0']['ISA']
+		self.misa = isa_yaml['hart0']['misa']
+		
+		
+		if 'RV32' in self.isa:
+            		self.isa_bit = 'rv32'
+            		self.xlen = 32
+            		self.offset_inc = 4
+		else:
+            		self.isa_bit = 'rv64'
+            		self.xlen = 64
+            		self.offset_inc = 8
+            	
+		
+		return True
+		
+	def generate_asm(self) -> List[Dict[str, Union[Union[str, list], Any]]]:
+        
+		asm=f'\tauipc x2,0x0004\n'                                         #making pc 4 byte aligned
+		asm+=f'\tcsrrc x7,misa, 0x4\n'                                     # disables C ext;x7 willl have old misa value
+		asm+=f'\c.li x7,0x500\n'                                           #should raise interrupt
+		asm+=f'\tauipc x2,0x0002\n'                                        #making pc 2 byte aligned
+		asm+=f'\tcsrrc x7,misa, 0x4\n'                                     # ignores disabling C ext 
+		asm+=f'\tc.li x7,0x500 \n'                                         #no interrupt
+		          
+		compile_macros = []
+		 
+		return [{
+		 'asm_code': asm,
+		 'asm_sig': '',
+		'compile_macros' : compile_macros
+		}] 
+        
+        	
+	def check_log(self, log_file_path, reports_dir) -> bool:
+		return False
+
+	def generate_covergroups(self, config_file) -> str:
+		sv = ""
+		return sv
+
+
+
---
+++ uatg_csrbox_warl_test_mtvec.py
@@ -0,0 +1,56 @@
+
+from yapsy.IPlugin import IPlugin
+from ruamel.yaml import YAML
+import uatg.regex_formats as rf
+from typing import Dict, Union, Any, List
+import re
+import os
+
+class uatg_csrbox_warl_test_mtvec(IPlugin):
+	def __init__(self):
+		super().__init__()
+		pass
+		
+	def execute(self, core_yaml, isa_yaml):
+		self.isa = isa_yaml['hart0']['ISA']
+		self.mtvec = isa_yaml['hart0']['mtvec']
+		if 'RV32' in self.isa:
+			self.isa_bit = 'rv32'
+			self.xlen = 32
+			self.offset_inc = 4
+		else:
+			self.isa_bit = 'rv64'
+			self.xlen = 64
+			self.offset_inc = 8
+			    	
+		self.bitmask = isa_yaml['hart0']['mtvec']['rv64']['base']['type']['warl']['legal']       #assuming bitmask is obtained
+		self.default_val = isa_yaml['hart0']['mtvec']['reset-val']
+				 
+		return True
+		
+	def generate_asm(self) -> List[Dict[str, Union[Union[str, list], Any]]]:
+		
+		asm=f'\tli x4,0xfffffffffffffffff\n'                    #load test value to x4
+		asm+=f'\tand x5,x4,{self.bitmask}\n'                   #test if x4 is legal value
+		asm+=f'\tneg {self.bitmask},{self.bitmask}\n'
+		asm+=f'\tand x6,{self.default_val},{self.bitmask}\n'
+		asm+=f'\tori x7,x5,x6\n'                                #(write_val & bitmask) | (default_val &~bitmask)   
+		asm+=f'\tbeq x7,{self.bitmask},loop\n'
+		asm+=f'loop:\n'
+		asm+=f'\tcsrrw x5,mtvec[61:0],x4\n'                      #write to mtvec if value is legal   
+			 
+		compile_macros = []
+			 
+		return [{
+		'asm_code': asm,
+		'asm_sig': '',
+		'compile_macros' : compile_macros
+		}] 
+		
+        	
+	def check_log(self, log_file_path, reports_dir) -> bool:
+		return False
+
+	def generate_covergroups(self, config_file) -> str:
+		sv = ""
+		return sv
